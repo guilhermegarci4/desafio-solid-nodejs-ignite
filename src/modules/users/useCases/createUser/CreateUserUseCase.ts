6df29f874b08680f7311908b62cb8e27a648c433
Guilherme Garcia <guillherme966@gmail.com>
2021-03-22 20:17:02 -0300

    First commit e complete

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const emailAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExists) {
+      throw new Error("Email already exist");
+    }
+
+    if (email === "" || name === "") {
+      throw new Error("Field all the fields");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
